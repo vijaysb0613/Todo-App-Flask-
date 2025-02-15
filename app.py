c8cbce1e4673a09c24cc03b4b4c23fdd7320e390
vijaysb0613 <vijaysb2006@gmail.com>
2025-02-15 12:38:51 +0530

    Updated CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime  
 
@@ -17,9 +17,19 @@ class Todo(db.Model):
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        pass
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+    
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error adding Task"
     else:
-        return render_template('index.html')
-    return render_template('index.html')
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html' , tasks=tasks)
+    return render_template('index.html',tasks=tasks)
 if __name__== "__main__":
     app.run(debug=True)
