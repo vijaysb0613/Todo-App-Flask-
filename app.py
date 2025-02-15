79275e5c5ac1aaf5ee33556490cae19b80523227
vijaysb0613 <vijaysb2006@gmail.com>
2025-02-15 12:57:16 +0530

    Update Delelte Route

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from ast import Delete
 from flask import Flask,render_template,url_for,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime  
@@ -30,6 +31,15 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html' , tasks=tasks)
-    return render_template('index.html',tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id): 
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "there was a problem deleteing"
 if __name__== "__main__":
     app.run(debug=True)
